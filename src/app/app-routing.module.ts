9bffa40480dfa2d7b30802d03be403d496b132f0
Amit Hinduja <ahinduja@isystango.com>
2021-05-26 20:01:13 +0530

    Ading code to Angular Boilerplate

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {AuthGuard} from './guard/auth.guard'
+const routes: Routes = [
+  {path:'auth',loadChildren:()=>import('./all-modules/auth/auth.module').then((m)=>m.AuthModule)},
+  {path:'',loadChildren:()=>import('./all-modules/pages/pages.module').then((m)=>m.PagesModule),canActivate:[AuthGuard]},  
+  // wild card router
+  {path:'**',component:PageNotFoundComponent,data:{'title':"Page Not Found"}}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+    import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+    import { filter } from 'rxjs/operators';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  constructor(){ }
+  ngOnInit() {}
+}
